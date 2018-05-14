d1faabbb7c4b40bec0a7ff7739e325b49bc4edb1
sudakovilia <sudakovilia@gmail.com>
2018-05-14 20:39:12 +0300

    simplified normalize function

--- Classification/code.py
+++ Classification/code.py
@@ -4,15 +4,13 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 def normalize(data_frame):
     values = data_frame.drop(['class'], axis=1)
-    classes = data_frame[['class']]
     values = (values - values.mean())/values.std()
-    values['class'] = classes['class']
+    values['class'] = data_frame['class']
     return values
 
 
-data = pd.read_excel('data.xlsx')
-data_name = data[['name']]
-data = data.drop(['name'], axis=1)
+source_data = pd.read_excel('data.xlsx')
+data = source_data.drop(['name'], axis=1)
 data.fillna(0, inplace=True)
 data['class'] = data['class'].astype('int32')
 data = normalize(data)
@@ -29,7 +27,7 @@ predicted_classes = clf.predict(np.array(prediction_sample))
 prediction_sample['class'] = predicted_classes
 
 result = prediction_sample.append(learning_sample)
-result['name'] = data_name['name']
+result['name'] = source_data['name']
 result = result.sort_index()
 
 result.to_excel('result.xlsx', sheet_name='result')
